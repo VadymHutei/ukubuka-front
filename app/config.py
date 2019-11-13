c525c02dacb9cdc7dc809930845aa4d84ca2393c
Vadym Hutei <hutei@live.com>
2019-11-13 08:19:54 +0200

    [CONFIG]

---
+++ app/config.py
@@ -0,0 +1,10 @@
+try:
+    from config_custom import *
+except:
+    from config_default import *
+from db import DB
+
+# languages
+LANGUAGES = []
+LANGUAGES_DATA = []
+DEFAULT_LANGUAGE = False
---
+++ app/config_default.py
@@ -0,0 +1,34 @@
+APP_CONFIG = {
+    'debug': True,
+    'host': '0.0.0.0',
+    'port': 80
+}
+
+SITE_NAME = 'My Site'
+
+DB_PARAMS = {
+    'host': 'localhost',
+    'user': 'user_name',
+    'password': 'password',
+    'db': 'db_name',
+    'charset': 'utf8mb4'
+}
+DB_PREFIX = 'prfx_'
+
+SESSION_COOKIE_NAME = 'sessid'
+SESSION_COOKIE_EXPIRES = 90 # days
+SESSION_ID_AVAILABLE_CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyz'
+SESSION_ID_SIZE = 64
+SESSION_ID_GENERATION_ATTEMPTS = 10
+
+# user identification cookie
+UIC_NAME = 'uic'
+UIC_EXPIRES = 90 # days
+UIC_AVAILABLE_CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyz'
+UID_SIZE = 64
+
+GLOBAL_SALT = 'GLOBAL_SALT'
+SALT_AVAILABLE_CHARACTERS = '1234567890abcdefghilklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# users groups
+USERS_GROUPS = {1: 'admin', 2: 'customer'}
