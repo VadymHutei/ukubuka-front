6c3446417f102899f73e8cfe308dcaa962e4705a
Vadym Hutei <hutei@live.com>
2019-11-19 08:51:54 +0200

    [CONTENT SERVICE] get request

--- app/connect/content_service.py
+++ app/connect/content_service.py
@@ -3,14 +3,36 @@ import requests
 import config
 from core import AbstractConnect
 
+class ContentServiceError(Exception):
+    pass
+
 class ContentService(AbstractConnect):
 
     def __init__(self):
         self.url = config.CONTENT_SERVICE['url']
 
     def get(self, entity, **params):
-        response = requests.get(
-            self.url + entity,
-            params=params
-        )
-        return response.json()
+        try:
+            response = requests.get(
+                self.url + entity,
+                params=params,
+                timeout = (config.CONNECT_TIMEOUT, config.READ_TIMEOUT),
+                headers = {
+                    'Accept': 'application/json'
+                }
+            )
+        except (requests.exceptions.ConnectTimeout,
+                requests.exceptions.ReadTimeout,
+                requests.exceptions.ConnectionError,
+                requests.exceptions.HTTPError,
+                requests.RequestException) as e:
+            if config.MODE == 'dev':
+                raise e
+            else:
+                return False
+        else:
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print(response.status_code)
+                raise ContentServiceError
