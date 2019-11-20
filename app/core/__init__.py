eb20272a49f9ea9ef53159aa0f23e6f240618fcf
Vadym Hutei <hutei@live.com>
2019-11-20 07:56:28 +0200

    [MENU] deleted abstract menu

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -2,7 +2,6 @@ from core.abstract_handler import AbstractHandler
 from core.abstract_view import AbstractView
 from core.abstract_template import AbstractTemplate
 from core.abstract_layout import AbstractLayout
-from core.abstract_menu import AbstractMenu
 from core.abstract_connect import AbstractConnect
 
 from core.template import Template
--- app/core/abstract_menu.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import config
-
-class AbstractMenu(ABC):
-
-    @abstractmethod
-    def __iter__(self):
-        pass
