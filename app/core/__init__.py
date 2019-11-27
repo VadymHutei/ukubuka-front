f7ecf07cf22aa548d448cbd83c66bb1b6bd11381
Vadym Hutei <hutei@live.com>
2019-11-27 08:18:40 +0200

    [ABSTRACT VIEW]

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,9 +1,9 @@
 from core.abstract_handler import AbstractHandler
-from core.abstract_view import AbstractView
 from core.abstract_template import AbstractTemplate
+from core.template import Template
+from core.abstract_view import AbstractView
 from core.abstract_layout import AbstractLayout
 from core.abstract_connect import AbstractConnect
 
-from core.template import Template
 from core.layout import Layout
 from core.menu import Menu
--- app/core/abstract_view.py
+++ app/core/abstract_view.py
@@ -1,7 +1,22 @@
 from abc import ABC, abstractmethod
 
+from flask import render_template
+
+from core import Template
+
 class AbstractView(ABC):
+
+    _params = {}
 
     @abstractmethod
     def render(self):
         pass
+
+    def _setParam(self, name, value):
+        self._params[name] = value
+
+    def _setTemplate(self, template):
+        self.template = Template(template)
+
+    def _render(self):
+        return render_template(self.template.getPath(), **self._params)
