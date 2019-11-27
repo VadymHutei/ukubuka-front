1f22ed9facc5d342e3ea39a456fb590b816585c2
Vadym Hutei <hutei@live.com>
2019-11-27 08:19:29 +0200

    [ERROR HANDLER]

--- app/handlers/error.py
+++ app/handlers/error.py
@@ -1,14 +1,9 @@
-from core import AbstractHandler, responces
 from views import ErrorView
-import config
 
 
-class ErrorHandler(AbstractHandler):
+class ErrorHandler():
 
-    def __init__(self, code):
-        self.code = code
-        self.view = ErrorView()
-
-    def getPage(self):
-        page = str(self.code)
-        return self.view.render(page=page)
+    def getPage(self, code):
+        page = str(code)
+        view = ErrorView()
+        return view.render(page=page)
