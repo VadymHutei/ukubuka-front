ae6f56a25f773c0b751136a2a3a6f0ada6b9ccba
Vadym Hutei <hutei@live.com>
2020-01-27 09:18:18 +0200

    [CATEGORY]

--- app/domains/category.py
+++ app/domains/category.py
@@ -1,10 +1,23 @@
 from entities import Domain
+from core import validator
 
 
 class Category(Domain):
 
-    def __init__(self, data):
-        self.data = data
+    _fields = validator.getFieldsFor('category')
+    _validate_methods = validator.getFieldsFor('category')
 
-    def toList(self):
-        return self.data
+    def __init__(self, data):
+        self._data = {}
+        self._fillFields(data)
+
+    def _setField(self, field, value):
+        if field not in self._fields: return False
+        if not self._validate_methods[field](value): return False
+        self._data[field] = value
+
+
+    def _fillFields(self, data):
+        for field in data:
+            value = data[field]
+            self._setField(field, value)
