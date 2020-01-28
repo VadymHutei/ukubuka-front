9b7233cc6a4f0fd7a7197b111361e7e6ec1469e5
Vadym Hutei <hutei@live.com>
2020-01-28 09:11:29 +0200

    [CORE] __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,5 +1,6 @@
 from core.entity import Entity
 
+
 from core.abstract_handler import AbstractHandler
 from core.abstract_template import AbstractTemplate
 from core.template import Template
@@ -7,7 +8,6 @@ from core.abstract_view import AbstractView
 from core.abstract_layout import AbstractLayout
 from core.abstract_connect import AbstractConnect
 
-from core.validator import validator
 from core.layout import Layout
 from core.menu import Menu
 from core.response import Response
