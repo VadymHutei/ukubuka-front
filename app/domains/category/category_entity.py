677b4b33af03750116161d66ffa40ef507a97035
Vadym Hutei <hutei@live.com>
2020-01-29 08:15:20 +0200

    [CATEGORY]

--- app/domains/category/category_entity.py
+++ app/domains/category/category_entity.py
@@ -6,11 +6,11 @@ from core.validator.category import categoryID, categoryName
 class CategoryEntity(Entity):
 
     _validate_methods = {
-        'id': m.categoryID,
-        'parent': m.categoryID,
-        'name': m.categoryName,
-        'order': m.order,
-        'is_active': m.active
+        'id': categoryID,
+        'parent': categoryID,
+        'name': categoryName,
+        'order': order,
+        'is_active': active
     }
     _fields = set(_validate_methods.keys())
 
