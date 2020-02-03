392e0c87d8f40fcedd83dceabef0db184287e952
Vadym Hutei <hutei@live.com>
2020-02-03 08:41:34 +0200

    [ABSTRACT CLASSES]

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,14 +1,8 @@
 from core.entity import Entity
 
-
-from core.abstract_handler import AbstractHandler
-from core.abstract_template import AbstractTemplate
-from core.template import Template
-from core.abstract_view import AbstractView
-from core.abstract_layout import AbstractLayout
+from core.view import View
 from core.abstract_connect import AbstractConnect
 
-from core.layout import Layout
 from core.menu import Menu
 from core.response import Response
 from core.error import Error
--- app/core/abstract_handler.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class AbstractHandler(ABC):
-
-    @abstractmethod
-    def getPage(self):
-        pass
