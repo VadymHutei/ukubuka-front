7857aca5f73a17df5500ae507e24f51c685de792
Vadym Hutei <hutei@live.com>
2020-02-13 08:22:56 +0200

    [VALIDATOR]

--- app/core/validator/category.py
+++ app/core/validator/category.py
@@ -1,15 +1,14 @@
 import re
 
 
-def categoryID(category_id) -> bool:
+def categoryID(category_id):
     if isinstance(category_id, str):
         return bool(re.fullmatch(r'[0-9]{1,8}', category_id))
     if isinstance(category_id, int):
         return 1 <= len(str(category_id)) <= 8
     return False
 
-
-def categoryName(name) -> bool:
+def categoryName(name):
     if isinstance(name, str):
         return re.fullmatch(r'\w{1,64}', name)
     return False
